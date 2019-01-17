313aeb93d22f1b6634db162bc651c43c532cc45e
Eduardo Gonzalez <EduGonO@users.noreply.github.com>
2019-01-16 19:51:47 -0500

    Add

--- warmup.py
+++ warmup.py
@@ -1,32 +1,54 @@
 import numpy as np
 
-# Numpy
+# Numpy warmup
+# @Eduardo Gonzalez
+
+print("")
+print("Numpy Warmup")
+print("By: Eduardo Gonzalez")
+print("")
+
 
 # 1.
+print("1.")
 x = np.eye(3)
 print(x)
 print("")
  
 # 2.
+print("2.")
 x = np.random.randint(10, size=(3, 3))
 print(x)
 print("")
 
 # 3.
+print("3.")
 x = np.random.randint(10, size=(9, 9))
 print(x)
+print("")
 print("Min: ", x.min())
 print("Max: ", x.max())
 print("")
 
 # 4.
-x = np.zeros((9,9))
+print("4.")
+x = np.ones((3,3))
 print(x)
 
 # 5.
+print("5.")
 
+
+print("")
 
 # 6.
+print("6.")
 
+
+print("")
 
 # 7.
+print("7.")
+
+
+print("")
