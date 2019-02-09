cc941b323a263925cadf96e9a79dfca2b479dba1
Eduardo Gonzalez <320ego@gmail.com>
2019-02-09 16:33:34 -0500

    d

--- Gonzalez_Eduardo_ps1.py
+++ Gonzalez_Eduardo_ps1.py
@@ -3,10 +3,11 @@ from numpy import matlib
 from scipy import misc
 import matplotlib.pyplot as plt
 import cv2
+import os
 
 # Homework 1
 # @Eduardo Gonzalez
-
+'''
 m = np.array([[1,2, 3],[4, 5, 6],[7, 8, 9]])
 
 a = np.array([1, 2, 3])
@@ -59,7 +60,6 @@ cv2.imshow("image 2",image2) # Display image
 im1 = cv2.normalize(image1, np.zeros((500, 500)), 0, 1, cv2.NORM_MINMAX)
 im2 = cv2.normalize(image2, np.zeros((500, 500)), 0, 1, cv2.NORM_MINMAX)
 
-
 # Cut the images in halves
 i1 = im1[:, :250]
 i2 = im2[:, 250:]
@@ -68,6 +68,7 @@ halves = np.concatenate((i1, i2), axis=1)
 
 cv2.imwrite('halves.jpg', halves)
 
+# One and one
 new = np.empty((500, 500))
 for row in range(len(im1)):
     if row % 2 == 1: # es par
@@ -76,5 +77,53 @@ for row in range(len(im1)):
         new[row] = im2[row]
 
 cv2.imwrite(new)
+'''
+folder = os.listdir('George_W_Bush')
 
-grey = cv2.cvtColor(new, cv2.COLOR_BGR2GRAY)
+# to store average later
+average_array = np.zeros((250, 250, 3), dtype=np.float64)
+
+# images_array requires an array that holds the multiple arrays of the images
+images_array = np.float64(np.array([np.array(io.imread('George_W_Bush/' + fname)) for fname in folder]))
+
+# use numpy's provided mean function to calculate average (average_array =  average_array + images_array / total
+# would have worked as well ; don't forget to cast back into uint8 in order to properly display images)
+# average_array = np.array(np.mean(images_array, axis=0))
+average_array_type = np.array(np.mean(images_array, axis=0), dtype=np.uint8)
+# plt.imshow(average_array, cmap=plt.cm.gray)
+# plt.show()
+# plt.imshow(average_array_type, cmap=plt.cm.gray)
+# plt.show()
+
+# saving image
+# io.imsave('average_image_result.jpg', average_array_type)
+
+# to display all the images on plots
+plt.figure(figsize=(20, 4))
+plt.subplot(141)
+
+# Added together
+plt.imshow(smashed, cmap=plt.cm.gray)
+plt.axis('off')
+plt.title('added together', fontsize=20)
+plt.subplot(142)
+
+# 2 crops together
+plt.imshow(newImage, cmap=plt.cm.gray)
+plt.axis('off')
+plt.title('2 crops together', fontsize=20)
+plt.subplot(143)
+
+# For every row
+plt.imshow(frankensteinImage2, cmap=plt.cm.gray)
+plt.axis('off')
+plt.title('2 birds in one', fontsize=20)
+plt.subplot(144)
+
+# Average Face
+plt.imshow(average_array_type)
+plt.axis('off')
+plt.title('the average face', fontsize=20)
+plt.subplot(144)
+
+plt.show()
