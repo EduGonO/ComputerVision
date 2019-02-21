38b4e4af437946f8811639c9c58c2196627d9f68
Eduardo Gonzalez <320ego@gmail.com>
2019-02-21 12:42:29 -0500

    fe

--- PointProcess.py
+++ PointProcess.py
@@ -8,7 +8,7 @@ import numpy as np
 from scipy import ndimage
 from scipy import misc
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+import matplotlib.image as mpimg 
 
 
 from mpl_toolkits.mplot3d import Axes3D
