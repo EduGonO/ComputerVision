77d0f13287b335c870d68b9d63777ffe79b9f621
Eduardo Gonzalez <320ego@gmail.com>
2019-04-29 11:27:25 -0400

    m

--- homework3/code/my_imfilter.py
+++ homework3/code/my_imfilter.py
@@ -10,36 +10,28 @@ from scipy import ndimage
 import matplotlib.pyplot as plt
 from scipy import misc
 
+image = misc.imread('/Users/edu/github/ComputerVision/homework3/data/einstein.bmp', flatten=1)
+image = np.float64(image)
 
+#[intput_row, intput_col] = size(image(mslice[:], mslice[:], 1))
+#[filter_row, filter_col] = size(filter)
 
-#% This function is intended to behave like the built in function ndimage.filter. 
-#% While terms like "filtering" and
-#% "convolution" might be used interchangeably, and they are indeed nearly
-#% the same thing, there is a difference:
-#% from 'help filter2'
-#%    2-D correlation is related to 2-D convolution by a 180 degree rotation
-#%    of the filter matrix.
-#
-#% Your function should work for color images. Simply filter each color
-#% channel independently.
-#
-#% Your function should work for filters of any width and height
-#% combination, as long as the width and height are odd (e.g. 1, 7, 9). This
-#% restriction makes it unambigious which pixel in the filter is the center
-#% pixel.
-#
-#% Boundary handling can be tricky. The filter can't be centered on pixels
-#% at the image boundary without parts of the filter being out of bounds. If
-#% you look at 'help conv2' and 'help imfilter' you see that they have
-#% several options to deal with boundaries. You should simply recreate the
-#% default behavior of imfilter -- pad the input image with zeros, and
-#% return a filtered image which matches the input resolution. A better
-#% approach is to mirror the image content over the boundaries for padding.
-#
-#% % Uncomment if you want to simply call imfilter so you can see the desired
-#% % behavior. When you write your actual solution, you can't use imfilter,
-#% % filter2, conv2, etc. Simply loop over all the pixels and do the actual
-#% % computation. It might be slow.
-#% output = imfilter(image, filter);
+# Pad image with zeros (amount = minimum need of filter = half of row and
+# column
 
+pad_input_image = np.pad(image, pad_width=1, mode='constant', constant_values=0)
 
+plt.figure(1, figsize=(15, 5))
+plt.suptitle('Problem', fontsize=18)
+
+plt.subplot(1, 4, 1)
+plt.title('Image with pad')
+plt.imshow(pad_input_image, cmap=plt.cm.gray)
+
+gaussian = 1.0/256*np.array([[ 1,  4,  6,  4, 1],
+                             [ 4, 16, 24, 16, 4],
+                             [ 6, 24, 36, 24, 6],
+                             [ 4, 16, 24, 16, 4],
+                             [ 1,  4,  6,  4, 1]])
+
+print(gaussian)
---
+++ homework3/code/test.py
@@ -0,0 +1,59 @@
+import numpy
+# Only to read the image
+from scipy import misc
+from numpy.fft import fft2, ifft2, fftshift, ifftshift
+import math
+
+# Applies the gaussian filter with an already programed way of easyly doing a hig/lowPass
+def gaussian_blur_kernel_2d(row, col, sigma, high):
+
+    if row % 2 == 1:
+        vCenter = int(row/2) + 1 
+    else:
+        hCenter = int(row/2)
+
+    if col % 2 == 1:
+        hCenter = int(col/2) + 1
+    else:
+        hCenter = int(col/2)
+
+    def gaussian(v,h):
+
+        # ( -1 * (v-center^2 + h-center^2) ) / 2*sig^2
+        coeff = math.exp(-1 * ((v - vCenter)**2 + (h - hCenter)**2) / (2 * sigma**2))
+      
+        if high:
+            return (1-coeff)
+        else:
+            return coeff
+    
+    x = numpy.array([[gaussian(v,h) for h in range(col)] for v in range(row)])
+
+    return x
+
+# We use this to filter the matrix
+def dft(matrix, matrix2):
+   nDft = fftshift(fft2(matrix)) * matrix2
+   return ifft2(ifftshift(nDft))
+
+# This function creates a Low Pass Image
+def lowPass(matrix, sigma):
+   a, b = matrix.shape
+   return dft(matrix, gaussian_blur_kernel_2d(a, b, sigma, high=False))
+
+# This function creates a High Pass Image
+def highPass(matrix, sigma):
+   a, b = matrix.shape
+   return dft(matrix, gaussian_blur_kernel_2d(a, b, sigma, high=True))
+
+
+def hybridImage(highImgage, lowImgage, sigmaHigh, sigmaLow):
+   high = highPass(highImgage, sigmaHigh)
+   low = lowPass(lowImgage, sigmaLow)
+   return high + low
+
+p1 = misc.imread("/Users/edu/github/ComputerVision/homework3/data/cat.bmp", flatten=True)
+p2 = misc.imread("/Users/edu/github/ComputerVision/homework3/data/dog.bmp", flatten=True)
+
+hybrid = numpy.real(hybridImage(p1, p2, 25, 10))
+misc.imsave("hybridImage.png", hybrid)
